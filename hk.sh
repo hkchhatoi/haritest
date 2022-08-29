834751a1ca1b9e9ca1b97b0aea65762bd788889a
hkchhatoi <krushna.cht@gmail.com>
2022-08-29 15:46:46 +0530

    hktest

--- hk.sh
+++ hk.sh
@@ -1,4 +1,3 @@
 example of using arguments to a script
-echo "My first name is $1"
-echo "My surname is $2"
-echo "Total number of arguments is $#" 
+echo "My first name is 1"
+echo "My surname is 2"
